9eb563d6d19e41b1d848e7c23f0a54c837965c95
user <user@musiclibrary.local>
2026-07-03 23:05:32 +0700

    debug: tampilkan error detail dari files API

--- api/files.js
+++ api/files.js
@@ -24,6 +24,6 @@ module.exports = async (req, res) => {
     return res.status(200).json({ files })
   } catch (err) {
     console.error('List files error:', err)
-    return res.status(500).json({ error: 'Failed to list files' })
+    return res.status(500).json({ error: 'Failed to list files', detail: err.message })
   }
 }
